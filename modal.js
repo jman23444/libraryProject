d77f30ac5d7445ca1a8357897b1ae69d976adc5b
Jman23444 <jaxonhicks@gmail.com>
2025-02-21 13:10:15 -0800

    feat: Built Modal and its toggle functionality
    - Built HTML structure for modal
    - Styled Modal
    - Added JS to toggle its visibility

---
+++ modal.js
@@ -0,0 +1,37 @@
+
+document.addEventListener("DOMContentLoaded", function () {
+    //
+    const inputs = document.querySelectorAll("input, textarea");
+    //
+    const modalContainer = document.querySelector('.modal-container');
+    const closeModal = document.querySelector('.close-modal');
+    const createBook = document.querySelector('#create-book');
+
+    // Invalid / Valid class toggle functionality
+    inputs.forEach((input) => {
+        input.addEventListener("input", function () {
+            if (this.checkValidity()) {
+                this.classList.remove("invalid");
+                this.classList.add("valid");
+            } else {
+                this.classList.remove("valid");
+                this.classList.add("invalid");
+            }
+        });
+    });
+
+    // Default Modal Setting
+    if (modalContainer.classList.contains('show')) {
+        modalContainer.classList.remove('show');
+    }
+
+    // open modal functionality 
+    createBook.addEventListener('click', () => {
+        modalContainer.classList.add('show');
+    });
+
+    // close modal functionality
+    closeModal.addEventListener('click', () => {
+        modalContainer.classList.remove('show');
+    });
+});
